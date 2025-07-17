8a9d0b00423705f8d60d9e463887da5776164a33
Kieran Geary <kieran.r.geary@gmail.com>
2025-07-17 14:36:08 +0100

    fix: only use new link logic for windows

--- python-oxidized-importer/build.rs
+++ python-oxidized-importer/build.rs
@@ -1,34 +1,30 @@
-use pyo3_build_config::{InterpreterConfig, get, use_pyo3_cfgs};
+use pyo3_build_config::{get, use_pyo3_cfgs, InterpreterConfig};
 
 // nabbed from pyo3-ffi
-fn emit_link_config(interpreter_config: &InterpreterConfig) -> Result<(), &'static str> {
-    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
-
-    println!(
-        "cargo:rustc-link-lib={link_model}{alias}{lib_name}",
-        link_model = if interpreter_config.shared {
-            ""
-        } else {
-            "static="
-        },
-        alias = if target_os == "windows" {
-            "pythonXY:"
-        } else {
-            ""
-        },
-        lib_name = interpreter_config.lib_name.as_ref().ok_or(
-            "attempted to link to Python shared library but config does not contain lib_name"
-        )?,
-    );
-
-    if let Some(lib_dir) = &interpreter_config.lib_dir {
-        println!("cargo:rustc-link-search=native={lib_dir}");
+fn emit_link_config(interpreter_config: &InterpreterConfig) -> Result<(), String> {
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS").map_err(|e| e.to_string())?;
+    if "windows" == target_os.as_str() {
+        println!(
+            "cargo:rustc-link-lib={link_model}{alias}{lib_name}",
+            link_model = if interpreter_config.shared {
+                ""
+            } else {
+                "static="
+            },
+            alias = "pythonXY:",
+            lib_name = interpreter_config.lib_name.as_ref().ok_or(
+                "attempted to link to Python shared library but config does not contain lib_name"
+            )?,
+        );
+        if let Some(lib_dir) = &interpreter_config.lib_dir {
+            println!("cargo:rustc-link-search=native={lib_dir}");
+        }
     }
 
     Ok(())
 }
 
-fn main() -> Result<(), &'static str>{
+fn main() -> Result<(), String> {
     use_pyo3_cfgs();
     emit_link_config(get())
 }
